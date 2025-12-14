123684f43a50c9ca8c16f79eab71f8918928d255
Muhammad Hassan <mhasssu100@gmail.com>
2025-12-14 20:45:26 +0500

    Add practice examples for functions using parameters and arguments

--- script.js
+++ script.js
@@ -312,26 +312,57 @@
 //  let heros = ["spiderman","batman","superman","actman","hulk","venom","greenman"];
 //  let mhero = heros.slice(1,5);
 //  console.log(mhero);
- 
+
 // let heros = ["spiderman","batman","superman","actman","hulk","venom","greenman"];
 //  heros.splice(2,0,10,11,12)
 // console.log(heros);
 //PRACTICE ARRAYS SHIFT/POP METHODS <====
 
-let arr = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
-arr.shift();
-arr.pop();
-console.log(arr);
-// PRACTICE ARRAYS SPLICE METHODS <=====
+// let arr = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// arr.shift();
+// arr.pop();
+// console.log(arr);
+// // PRACTICE ARRAYS SPLICE METHODS <=====
 
-let arr1 = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
-console.log(arr1);
-arr1.splice(2,1,"Ola")
-console.log(arr1);
-//PRACTICE ARRAYS PUSH & UNSHIFT METHODS <=====
-let arr2 = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
-arr2.push("Amazon");
-console.log(arr2);
-arr2.unshift("Amazon");
-console.log(arr2);
+// let arr1 = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// console.log(arr1);
+// arr1.splice(2, 1, "Ola")
+// console.log(arr1);
+// //PRACTICE ARRAYS PUSH & UNSHIFT METHODS <=====
 
+// let arr2 = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// arr2.push("Amazon");
+// console.log(arr2);
+// arr2.unshift("Amazon");
+// console.log(arr2);
+
+//PRACTICE FUCTIONS <========
+// function myFunction() {
+//     console.log("Hello");
+//     console.log("World");
+// }
+// myFunction();
+
+// function myfunction(msg) {
+//     console.log(msg);  //<===PARAMETER
+// } 
+// myfunction("This is Hassan")   //ARGUMENTS
+
+// function myfunction(m, n) {
+//     console.log(m, n);
+// }
+// myfunction(5, 5 + 10);
+
+// function myFunction(val) {
+//     prompt("val");  
+// }
+// function sum(x,y) {
+//     console.log(x*y);
+// }
+
+function sum (x,y){
+    s = x / y;
+    return s;
+}
+let val = sum(3,7);
+console.log(val);
